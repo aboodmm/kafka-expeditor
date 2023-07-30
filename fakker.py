d0998b3019134eb162fb3baf65ba962297e42a10
Abdurrahman <aboodmm@gmail.com>
2023-07-29 20:35:29 -0700

    update sample config and add yaml parsing, logging config

--- fakker.py
+++ fakker.py
@@ -1,1 +1,24 @@
 import yaml
+import logging
+import subprocess
+
+fakker_config = {}
+
+def init_logger():
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] - %(message)s',
+                         datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+    logging.info("Initializing zookeeper and kafka")
+
+def init_configs():
+    with open ("config.yml", "r") as config_file:
+         fakker_config = yaml.safe_load(config_file)
+
+
+if (__name__ == "__main__"):
+    init_logger()
+    init_configs()
+    logging.info(fakker_config)
+
+
+
+
