a3c28ed14cfb7b34c82fdb88b85242d396940d22
Abdurrahman <aboodmm@gmail.com>
2023-07-29 21:47:51 -0700

    finalize properties. add config checker and start zookeeper

--- fakker.py
+++ fakker.py
@@ -1,6 +1,7 @@
 import yaml
 import logging
-import subprocess
+import os
+import sys
 
 fakker_config = {}
 
@@ -10,14 +11,37 @@ def init_logger():
     logging.info("Initializing zookeeper and kafka")
 
 def init_configs():
-    with open ("config.yml", "r") as config_file:
-         fakker_config = yaml.safe_load(config_file)
+    with open ("./config.yml", "r") as config_file:
+        global fakker_config
+        fakker_config = yaml.safe_load(config_file)
+
+def validate_configs():
+    root_obj = fakker_config.get("fakker")
+    broker_str = root_obj.get("broker")
+    dir_str = root_obj.get("kafkadir")
+    if (root_obj is None):
+        sys.exit("validate_configs(): Improper config")
+    if (broker_str is None or len(broker_str.strip()) < 1):
+        sys.exit("validate_configs(): Improper config")
+    if (dir_str is None or len(dir_str.strip()) < 1):
+        sys.exit("validate_configs(): Improper config")
+    return 
+
+def get_kafka_dir():
+    return fakker_config.get("fakker").get("kafkadir")
+
+def start_zookeeper():
+    kafka_dir = get_kafka_dir()
+    bin_dir = kafka_dir + "/bin/"
+    config_dir = kafka_dir + "/config/"
+    os.popen(bin_dir + "zookeeper-server-start.sh", config_dir + "zookeeper.properties")
 
 
 if (__name__ == "__main__"):
     init_logger()
     init_configs()
-    logging.info(fakker_config)
+    validate_configs()
+    start_zookeeper()
 
 
 
