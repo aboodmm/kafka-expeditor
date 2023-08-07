3bd69c881a3f245144af9e13218b65e53d862752
Abdurrahman <aboodmm@gmail.com>
2023-08-07 11:04:04 -0700

    add kafka producer and connection code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fakker
 Eliminate the extremely painful labor of posting to kafka for manual testing
+
+
+### Configuration
+
+add the following to your login script
+
+```
+alias zookup='<path_to_kafka>/bin/zookeeper-server-start.sh <path_to_kafka>/config/zookeeper.properties'
+alias kafkaup='<path_to_kafka>/bin/kafka-server-start.sh <path_to_kafka>/config/server.properties'
+```
--- fakker.py
+++ fakker.py
@@ -1,48 +1,55 @@
 import yaml
 import logging
-import os
 import sys
+from kafka import KafkaProducer
+import uuid
+import glob
 
 fakker_config = {}
 
 def init_logger():
     logging.basicConfig(format='[%(asctime)s] [%(levelname)s] - %(message)s',
                          datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
-    logging.info("Initializing zookeeper and kafka")
 
 def init_configs():
     with open ("./config.yml", "r") as config_file:
         global fakker_config
         fakker_config = yaml.safe_load(config_file)
 
-def validate_configs():
+def retrieve_conf(key):
     root_obj = fakker_config.get("fakker")
-    broker_str = root_obj.get("broker")
-    dir_str = root_obj.get("kafkadir")
-    if (root_obj is None):
+    target_obj = root_obj.get(key)
+    return target_obj
+
+def validate_configs():
+    if (fakker_config is None):
         sys.exit("validate_configs(): Improper config")
+    broker_str = retrieve_conf("broker")
     if (broker_str is None or len(broker_str.strip()) < 1):
-        sys.exit("validate_configs(): Improper config")
-    if (dir_str is None or len(dir_str.strip()) < 1):
         sys.exit("validate_configs(): Improper config")
     return 
 
-def get_kafka_dir():
-    return fakker_config.get("fakker").get("kafkadir")
+def connect_kafka_producer():
+    return KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10))
 
-def start_zookeeper():
-    kafka_dir = get_kafka_dir()
-    bin_dir = kafka_dir + "/bin/"
-    config_dir = kafka_dir + "/config/"
-    os.popen(bin_dir + "zookeeper-server-start.sh", config_dir + "zookeeper.properties")
+def post_to_kafka(producer):
+    topics_dict = fakker_config.get("topics")
+    for topic,dir in topics_dict.items():
+        for curfile in glob.glob(dir + "/*"):
+            with open(curfile, "r") as file:
+                output_key = str(uuid.uuid1())
+                key_bytes = bytes(output_key, encoding="utf-8")
+                value_bytes = bytes(file.read(), encoding="utf-8")
+                producer.send(k, key=key_bytes, value=value_bytes)
+        print("test")
 
 
 if (__name__ == "__main__"):
     init_logger()
     init_configs()
     validate_configs()
-    start_zookeeper()
-
+    producer = connect_kafka_producer()
+    post_to_kafka(producer)
 
 
 
